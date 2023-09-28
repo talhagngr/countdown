4f0df6e7e18ecfa4cbd338dced21204a4a46975e
talhagngr <111577996+talhagngr@users.noreply.github.com>
2023-09-28 22:09:21 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+function setDate() {
+    const inputElement = document.getElementById(2023-05-19);
+    const countdownElement = document.getElementById('countdown');
+    const selectedDate = new Date(inputElement.value);
+    const currentDate = new Date();
+    
+    const diffTime = selectedDate - currentDate;
+    if(diffTime > 0) {
+        // Future Date
+        const days = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        countdownElement.innerHTML = days + ' days until ' + selectedDate.toDateString();
+    } else {
+        // Past Date
+        const days = Math.floor(Math.abs(diffTime) / (1000 * 60 * 60 * 24));
+        countdownElement.innerHTML = days + ' days since ' + selectedDate.toDateString();
+    }
+}
