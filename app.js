9dda5e375b681394fde5a1e408ca5e3bf030a3e2
talhagngr <111577996+talhagngr@users.noreply.github.com>
2023-09-30 02:04:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,25 +11,33 @@ document.addEventListener('DOMContentLoaded', function() {
         countdownElement.innerHTML = days + ' days have passed since 19 May 2023.';
     }
 
-  const checkButton = document.getElementById('checkButton');
-    const timerText = document.getElementById('timerText');
-   
-    // Log the initial state to the console.
-    console.log('Initial Consecutive Days:', localStorage.getItem('consecutiveDays') || 0);
-   
-    // When checkButton is clicked, increment consecutiveDays in Local Storage.
-    checkButton.addEventListener('click', function() {
-        let consecutiveDays = parseInt(localStorage.getItem('consecutiveDays') || 0);
-        console.log('Before Increment:', consecutiveDays); // Log before increment
-        consecutiveDays++;
-        localStorage.setItem('consecutiveDays', consecutiveDays.toString());
-        console.log('After Increment:', consecutiveDays); // Log after increment
-        updateTimerText();
-    });
+    const checkButton = document.getElementById('checkButton');
+        const timerText = document.getElementById('timerText');
+        const indicator = document.getElementById('indicator');
 
-    function updateTimerText() {
-        const consecutiveDays = localStorage.getItem('consecutiveDays') || 0;
-        timerText.innerHTML = `Timer: ${consecutiveDays} days`;
-    }
-    updateTimerText(); // Run this function once when the page loads to set initial text.
+        checkButton.addEventListener('click', function() {
+            const todayStr = new Date().toDateString();
+            const lastPressedStr = localStorage.getItem('lastPressed') || '';
+
+            if(todayStr !== lastPressedStr) {
+                let consecutiveDays = parseInt(localStorage.getItem('consecutiveDays') || 0);
+                consecutiveDays++;
+                localStorage.setItem('consecutiveDays', consecutiveDays.toString());
+                localStorage.setItem('lastPressed', todayStr);
+            }
+
+            updateUI();
+        });
+
+        function updateUI() {
+            const todayStr = new Date().toDateString();
+            const lastPressedStr = localStorage.getItem('lastPressed') || '';
+            const consecutiveDays = localStorage.getItem('consecutiveDays') || 0;
+
+            timerText.innerHTML = `Timer: ${consecutiveDays} days`;
+            indicator.className = todayStr === lastPressedStr ? 'green' : 'red';
+        }
+
+        updateUI(); // Run this function once when the page loads to set initial state.
+    });
 });
